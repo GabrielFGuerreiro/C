f3df8285afa680d292d7a560dd79388c1f84d915
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-08-03 14:28:53 -0300

    exercicio 2 adicionado

---
+++ exercicio2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    //Receba dois numeros e exiba o resultado da divisao do primeiro pelo segundo
+    int a,b,divi;
+    printf("Digite o primeiro numero\n");
+    scanf("%d",&a);
+        printf("Digite o segundo numero\n");
+    scanf("%d",&b);
+    divi=a/b;
+    printf("A divisao dos valores e %d \n",divi);
+}
