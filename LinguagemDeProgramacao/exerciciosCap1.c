536c00cdae8b89e790855afdc0bd2e0bbd6a156d
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-08-08 12:57:18 -0300

    Exercicio 2 cap 1

--- LinguagemDeProgramacao/exerciciosCap1.c
+++ LinguagemDeProgramacao/exerciciosCap1.c
@@ -22,3 +22,42 @@ int main(void)
     return 0;
 }
 #endif // ex1
+
+                        //==========//
+
+#ifdef ex2
+/*Escreva um programa que receba dados via teclado para variaveis do tipo short,long, int, float, double e um char e imprima-os no video no seguinte formato:
+             10        20        30        40        50        60
+     12345678901234567890123456789012345678901234567890123456789012345
+         short               long                int
+                   float               double              char*/
+int main(void){
+int num;
+float deci;
+double deci2;
+char charac;
+short int numshort;
+long int numlong;
+
+setlocale(LC_ALL,"");
+printf("Digite um número inteiro qualquer\n");
+scanf("%d",&num);
+
+printf("Digite dois números quaisquer\n");
+scanf("%f %lf",&deci,&deci2);
+
+printf("Digite um caracter qualquer\n");
+scanf(" %c",&charac);
+
+printf("Digite três números inteiros quaisquer\n");
+scanf("%hd %ld",&numshort,&numlong);
+
+printf("%5hd %25ld %45d\n%15.f %35.lf %55c\n",numshort,numlong,num,deci,deci2,charac);
+return 0;
+}
+
+#endif // ex2
+
+                        //==========//
+
+                        
