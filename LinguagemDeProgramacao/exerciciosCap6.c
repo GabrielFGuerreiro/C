8e15f325cfb621bdfe382e63763455b7fd228128
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-08-15 11:36:30 -0300

    Exercicio 2 cap 6

--- LinguagemDeProgramacao/exerciciosCap6.c
+++ LinguagemDeProgramacao/exerciciosCap6.c
@@ -59,3 +59,67 @@ char resp;
 }
 
 #endif // ex1
+
+
+#ifdef ex2
+//Reescreva o exercicio anterior utilizando operadores de atribuicao composta.
+
+char recomeca();
+
+int main(){
+int num1,num2,soma,sub,mult,a,b,c,d;
+float divi;
+char resp;
+
+do{
+
+setlocale(LC_ALL,"");
+printf("Digite dois números inteiros\n");
+scanf("%d",&num1);
+scanf("%d",&num2);
+
+soma=num1;
+soma+=num2;
+
+sub=num1;
+sub-=num2;
+
+mult=num1;
+mult*=num2;
+
+divi=num1;
+divi/=num2;
+
+printf("Soma:%d\nSubtração:%d\nMultiplicação:%d\nDivisão:%.2f\n",soma,sub,mult,divi);
+
+resp=recomeca();
+
+}while(resp=='S' || resp=='s');
+}
+
+
+char recomeca()
+{
+char resp;
+    do
+        {
+            printf("Deseja recomeçar?(S/N)\n");
+            resp=getch();
+            if(resp!='s' && resp!='S' && resp!='n' && resp!='N')
+            {
+                        printf("ERRO\n");
+            }
+        }
+        while(resp!='s' && resp!='S' && resp!='n' && resp!='N');
+
+
+        if(resp=='S' || resp=='s')
+        {
+            system("cls");
+        }
+        else
+            printf("\nAté logo!\n");
+
+        return resp;
+}
+#endif // ex2
