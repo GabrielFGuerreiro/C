adb016f3c79e557c99c0143be22bd4cb145f7d0a
GabrielFGuerreiro <gaferg2004@gmail.com>
2024-08-21 14:45:46 -0300

    Exercicio 2 cap 9

--- LinguagemDeProgramacao/exerciciosCap9.c
+++ LinguagemDeProgramacao/exerciciosCap9.c
@@ -105,3 +105,97 @@ x=(*pnum1)/(*pnum2);
 #endif // ex1
 
 
+#ifdef ex2
+/*Escreva um programa que receba uma letra via teclado usando ponteiro.
+Escreva uma funcao que pesquise se esta letra existe no vetor abaixo usando ponteiros.
+Imprima o resultado da pesquisa no video na funcao main(). Passe como informacao
+para a funcao a letra digitada e o vetor usando ponteiros e faca a pesquisa
+usando ponteiros.(utilize o comando return).
+O vetor deve ser declarado como variavel LOCAL na funcao main().
+
+vetor -> b,d,f,h,j,k,m,o,q,s,u,w,y*/
+
+pesquisa(char *pc,char *pvet);
+char saida();
+
+main()
+{
+char vetc[]={'b','d','f','h','j','k','m','o','q','s','u','w','y','\0'};
+char *pvet;
+pvet=&vetc[0];
+
+char c,resp;
+char *pc;
+pc=&c;
+
+int x;
+
+do{
+
+printf("Digite uma letra\n");
+scanf(" %c",pc);
+
+x=pesquisa(pc,pvet);
+
+if(x==1)
+{
+printf("A letra '%c' existe no vetor\n",*pc);
+}
+else
+{
+setlocale(LC_ALL,"");
+printf("A letra '%c' não existe no vetor\n",*pc);
+}
+
+resp=saida();
+
+}while(resp=='n' || resp=='N');
+}
+
+
+
+pesquisa(char *pc,char *pvet)
+{
+int cont=0,x=0;
+
+for(cont=0;*(pvet+cont)!='\0';cont++)
+{
+    if(*pc==*(pvet+cont))
+    {
+        return x=1;
+    }
+}
+
+return x=0;
+}
+
+
+
+char saida()
+{
+
+char resp;
+
+do
+{
+printf("Deseja encerrar o programa?(S/N)\n");
+resp=getch();
+if(resp!='s' && resp!='S' && resp!='n' && resp!='N')
+    {
+        printf("Valor inválido\n");
+    }
+}while(resp!='s' && resp!='S' && resp!='n' && resp!='N');
+
+
+if(resp=='S' || resp=='s')
+    {
+        printf("\nAté logo!\n");
+        exit(0);
+    }
+else
+    system("cls");
+
+return resp;
+}
+
+#endif // ex2
